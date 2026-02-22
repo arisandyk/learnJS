4445b010a67cbfbf2f573566f76f4be4fe907544
Ari Sandy Kurniawan <arisandykurniawan@Aris-MacBook-Pro.local>
2026-02-23 06:15:50 +0700

    Belajar Operator di JS selesai

---
+++ task2.js
@@ -0,0 +1,16 @@
+/*
+Daftar Materi:
+1. Operator pada JavaScript: Aritmatika (+, -, /, %, *), Penugasan (=, +=, -=, *=, %=, /= ), Perbandingan (==, ===, !=, !==, >, <, >=, <=)
+2. Operator pada JavaScript: Logika (&&, ||, !), String (+), Typeof, & Kondisional)
+3. Tipe Data pada JS: String
+*/
+
+// A. Operator pada JS
+let x = 12;
+let hasilPemerikasaan = (x %2 == 0);
+let y = typeof(x);
+console.log(hasilPemerikasaan);
+console.log(y);
+
+let operatorKondisional = (x %2 == 0)? "Genap":"Ganjil";
+console.log(operatorKondisional);
