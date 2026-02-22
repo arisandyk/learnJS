b4aa25037564fbc0dfac2210f217940da7e22783
Ari Sandy Kurniawan <arisandykurniawan@Aris-MacBook-Pro.local>
2026-02-23 06:03:48 +0700

    Initial Commit: Task Hari 1-3 Selesai

---
+++ task1.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+let r = 10;
+
+let luasLingkaran = PI * r * r;
+// console.log("Luas Lingkaran: " + luasLingkaran);
+
+// Soal HOTS
+let targetTabungan = 50000000;
+let uangSaatIni = 20000000;
+let tambahanBulanIni = 5000000;
+
+let totalUang = uangSaatIni + tambahanBulanIni;
+let sisaKekurangan = targetTabungan - totalUang;
+
+console.log(sisaKekurangan);
+
